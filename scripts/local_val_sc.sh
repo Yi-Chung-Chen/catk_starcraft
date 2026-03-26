4d098f449b2c82dbccaa34158336ce16091fcd81
Yi-Chung Chen <chen5262@purdue.edu>
2026-03-26 11:42:36 -0400

    Add sc val script

---
+++ scripts/local_val_sc.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export LOGLEVEL=INFO
+export HYDRA_FULL_ERROR=1
+export TF_CPP_MIN_LOG_LEVEL=2
+
+MY_EXPERIMENT="sc_pre_bc"
+MY_TASK_NAME="sc_closed_val_eval"
+MY_CKPT_PATH="logs/sc_pre_bc-debug/runs/2026-03-26_00-54-15/checkpoints/last.ckpt"
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate catk
+
+# Local StarCraft closed-loop validation on a single GPU.
+python \
+  -m src.run \
+  action=validate \
+  experiment=$MY_EXPERIMENT \
+  trainer=default \
+  trainer.accelerator=gpu \
+  trainer.devices=1 \
+  trainer.strategy=auto \
+  ckpt_path=$MY_CKPT_PATH \
+  model.model_config.val_closed_loop=true \
+  model.model_config.val_open_loop=false \
+  model.model_config.n_rollout_closed_val=1 \
+  trainer.limit_val_batches=10 \
+  task_name=$MY_TASK_NAME
+
+echo "bash local_val_sc.sh done!"
