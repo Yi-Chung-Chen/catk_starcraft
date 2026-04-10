ad946751fde632bf4d7bb8ed402e573e823253bc
Yi-Chung Chen <chen5262@purdue.edu>
2026-04-10 13:55:35 -0400

    Remove redundant train_sc.sbatch and add local training scripts for ablation experiments
      - Remove scripts_rcac/train_sc.sbatch (superseded by run.sbatch)
      - Add 6 local training scripts under scripts/ matching RCAC ablation setups
        (smart, hmart, hmart_aux, hmart_c8, hmart_c32, hmart_intent_aux)
      - Use batch_size=8 for A6000 memory, no trainer=ddp (strategy=auto suffices)

---
+++ scripts/train_hmart_c32.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export LOGLEVEL=INFO
+export HYDRA_FULL_ERROR=1
+export TF_CPP_MIN_LOG_LEVEL=2
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+# Ablation: hmart_c32 (global concepts=32, no aux)
+
+MY_EXPERIMENT="sc_pre_bc"
+MY_TASK_NAME="hmart_c32"
+
+torchrun --nproc_per_node=gpu \
+  -m src.run \
+  experiment=$MY_EXPERIMENT \
+  task_name=$MY_TASK_NAME \
+  data.train_batch_size=8 \
+  data.val_batch_size=8 \
+  data.test_batch_size=8 \
+  model.model_config.use_aux_loss=false \
+  model.model_config.decoder.num_concepts=32
+
+echo "train_hmart_c32.sh done!"
---
+++ scripts/train_hmart_intent_aux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export LOGLEVEL=INFO
+export HYDRA_FULL_ERROR=1
+export TF_CPP_MIN_LOG_LEVEL=2
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+# Ablation: hmart_intent_aux (global concepts + aux loss + intent input)
+
+MY_EXPERIMENT="sc_pre_bc"
+MY_TASK_NAME="hmart_intent_aux"
+
+torchrun --nproc_per_node=gpu \
+  -m src.run \
+  experiment=$MY_EXPERIMENT \
+  task_name=$MY_TASK_NAME \
+  data.train_batch_size=8 \
+  data.val_batch_size=8 \
+  data.test_batch_size=8 \
+  model.model_config.use_aux_loss=true \
+  model.model_config.decoder.use_action_target_input=true \
+  model.model_config.decoder.num_concepts=16
+
+echo "train_hmart_intent_aux.sh done!"
